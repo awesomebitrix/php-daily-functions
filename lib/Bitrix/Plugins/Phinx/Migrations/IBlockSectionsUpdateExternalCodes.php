93ad1974e08b0f9098a92cc5135e174cd57297f0
Stanislav Stepanov <sstepanov@articul.office>
2017-01-13 15:43:24 +0300

    [Import:FromXML] XML importer of IBlocks with defaults + small piece of documentation added

---
+++ lib/Bitrix/Plugins/Phinx/Migrations/IBlockSectionsUpdateExternalCodes.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace bfday\PHPDailyFunctions\Bitrix\Plugins\Phinx\Migrations;
+
+use Bitrix\Iblock\SectionTable;
+use Bitrix\Main\Entity\Query;
+
+class IBlockSectionsUpdateExternalCodes extends Base
+{
+    /**
+     * Modes. Don't forget update ::initCommonData() method after adding new mode.
+     */
+    const MODE__EQUAL_TO_ID          = 1;
+    const MODE__EQUAL_TO_CODE        = 2;
+    const MODE__UNIQUE_VALUE_BY_ID   = 3;
+    const MODE__UNIQUE_VALUE_BY_CODE = 4;
+
+    // settings
+
+    /**
+     * @var string - fills up manually
+     */
+    protected $iBlockCode;
+
+    /**
+     * @var int - fills up automatically
+     */
+    protected $iBlockId;
+
+    /**
+     * @var array - fills up manually, depends on $mode
+     */
+    protected $sectionsData;
+
+    /**
+     * @var mixed - fills up manually, depends on available modes
+     */
+    protected $mode;
+
+    /**
+     * @var int - number of sections to process per 1 cicle (to not occupy large memory piece)
+     */
+    protected $processSectionsPerStep = 3;
+
+    public function up()
+    {
+        parent::up();
+
+        $this->initSettings();
+
+        $sectionPropertyExtCode = "XML_ID";
+
+        switch ($this->mode) {
+            case static::MODE__EQUAL_TO_ID:
+                // get all sections 1 by 1
+                $sections = [];
+                $page = 1;
+                do {
+                    $sectionQuery = new Query(SectionTable::getEntity());
+                    $sections = $sectionQuery
+                        ->addSelect([
+                            "ID",
+                            "NAME",
+                            $sectionPropertyExtCode,
+                        ])
+                        ->addFilter([
+                            "IBLOCK_CODE" => $this->iBlockCode,
+                        ])
+                        ->setLimit($this->processSectionsPerStep)
+                        ->setOffset($page - 1)
+                    ;
+
+                    foreach ($sections as $section) {
+                        $this->getOutput()->writeln("Section ({$section['ID']} {$section['NAME']}) have been updated.");
+                        if ($section['ID'] != $section[$sectionPropertyExtCode]) {
+                            // update XML_ID of section
+                            $this->getOutput()->writeln("Section ({$section['ID']} {$section['NAME']}) have been updated.");
+                            /*SectionTable::update($section['ID'],
+                                [
+                                    $sectionPropertyExtCode => $section['ID'],
+                                ]
+                            );*/
+                        }
+                    }
+
+                    $page++;
+                } while (is_array($sections) && count($sections) >= $this->processSectionsPerStep);
+                // change their codes
+                throw new \Exception("test");
+                break;
+            case static::MODE__EQUAL_TO_CODE:
+                // ToDo: implement logic
+                throw new \Exception("Sorry, but selected [mode] have not been implemnted yet.");
+                break;
+            case static::MODE__UNIQUE_VALUE_BY_ID:
+                // ToDo: implement logic
+                throw new \Exception("Sorry, but selected [mode] have not been implemnted yet.");
+                break;
+            case static::MODE__UNIQUE_VALUE_BY_CODE:
+                // ToDo: implement logic
+                throw new \Exception("Sorry, but selected [mode] have not been implemnted yet.");
+                break;
+        }
+    }
+
+    public function down()
+    {
+        parent::down();
+
+        $this->initSettings();
+
+        $this->getOutput()
+             ->writeln("No reverse migration is needed.")
+        ;
+    }
+
+    /**
+     * Inits initial state of object.
+     *
+     * @throws \Exception
+     */
+    private function initSettings()
+    {
+        if (empty($this->iBlockCode) || !is_string($this->iBlockCode)) {
+            throw new \Exception("Property (iBlockCode) should have string type and cannot be empty.");
+        }
+
+        $this->iBlockId = \bfday\PHPDailyFunctions\Bitrix\Helpers\IBlock::getInstance()
+                                                                        ->getIBlockIDsByCodes($this->iBlockCode)[$this->iBlockCode];
+        if ($this->iBlockId === false) {
+            throw new \Exception("IBlock ({$this->iBlockCode}) not found.");
+        }
+
+        if (empty($this->mode) || !in_array($this->mode, [
+                static::MODE__EQUAL_TO_ID,
+                static::MODE__EQUAL_TO_CODE,
+                static::MODE__UNIQUE_VALUE_BY_ID,
+                static::MODE__UNIQUE_VALUE_BY_CODE,
+            ])
+        ) {
+            throw new \Exception("Not supported value for property [mode]. Explicit.");
+        }
+
+        $this->processSectionsPerStep = intval($this->processSectionsPerStep);
+        if ($this->processSectionsPerStep <= 0) {
+            throw new \Exception("Not supported value for property [processSectionsPerStep]. It must have int value greater than 0.");
+        }
+
+        \Bitrix\Main\Loader::includeModule('iblock');
+    }
+}
